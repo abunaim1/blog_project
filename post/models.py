d82a30b7a75d56af2d35d2bb2bf53d80649cf5c8
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-21 01:55:32 +0600

    error solved

--- post/models.py
+++ post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from categories.models import Category
+from author.models import Author
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    
+    # making relationships
+    category = models.ManyToManyField(Category) # A post could be create by many categories and also Many post could be create base on a category. so this is many to many relationship
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+"""
+1. create project
+2. create app
+3. link app and all urls
+4. make all model
+5. do all admin setup
+6. 
+"""
